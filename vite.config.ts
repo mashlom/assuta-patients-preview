e58db13207bed8904b3cbe6d981a47ffdeb40c91
relbns <ariel@stoketalent.com>
2024-09-29 16:34:32 +0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -7,13 +7,21 @@
 //     outDir: 'dist',
 //   },
 // });
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
 
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, '.')
   return {
     plugins: [react()],
     base: env.PR_NUMBER ? `/pr-preview/${env.PR_NUMBER}/` : '/',
-  };
-});
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+  }
+})
