6ae05fdba3ac597e70553964ec5aae4f08e31a30
relbns <ariel@stoketalent.com>
2024-09-29 17:00:18 +0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,30 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
+// // import { defineConfig } from 'vite';
+// // import react from '@vitejs/plugin-react';
 
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     outDir: 'dist',
-//   },
-// });
+// // export default defineConfig({
+// //   plugins: [react()],
+// //   build: {
+// //     outDir: 'dist',
+// //   },
+// // });
 
+// import { defineConfig, loadEnv } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import path from 'path'
+
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => {
+//   const env = loadEnv(mode, '.')
+//   return {
+//     plugins: [react()],
+//     base: env.PR_NUMBER ? `/pr-preview/${env.PR_NUMBER}/` : '/',
+//     resolve: {
+//       alias: {
+//         '@': path.resolve(__dirname, './src'),
+//       },
+//     },
+//   }
+// })
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
@@ -15,13 +32,18 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.')
+  const base = env.PR_NUMBER ? `/pr-preview/${env.PR_NUMBER}/` : '/'
+  
   return {
     plugins: [react()],
-    base: env.PR_NUMBER ? `/pr-preview/${env.PR_NUMBER}/` : '/',
+    base,
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
       },
+    },
+    build: {
+      outDir: env.PR_NUMBER ? `dist/pr-preview/${env.PR_NUMBER}` : 'dist',
     },
   }
 })
