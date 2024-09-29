cd4e66375675813be11fbde2affe25ecebe018cf
relbns <relbns@users.noreply.github.com>
2024-09-29 14:20:40 +0000

    deploy: d7e95112b2dc4c108548f56bc330a4daa5cc6121

---
+++ vite.config.ts
@@ -0,0 +1,49 @@
+// // import { defineConfig } from 'vite';
+// // import react from '@vitejs/plugin-react';
+
+// // export default defineConfig({
+// //   plugins: [react()],
+// //   build: {
+// //     outDir: 'dist',
+// //   },
+// // });
+
+// import { defineConfig, loadEnv } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import path from 'path'
+
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => {
+//   const env = loadEnv(mode, '.')
+//   return {
+//     plugins: [react()],
+//     base: env.PR_NUMBER ? `/pr-preview/${env.PR_NUMBER}/` : '/',
+//     resolve: {
+//       alias: {
+//         '@': path.resolve(__dirname, './src'),
+//       },
+//     },
+//   }
+// })
+
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const base = env.PR_NUMBER ? `/pr-preview/${env.PR_NUMBER}/` : '/'
+  
+  return {
+    plugins: [react()],
+    base,
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    build: {
+      outDir: 'dist',
+    },
+  }
+})
